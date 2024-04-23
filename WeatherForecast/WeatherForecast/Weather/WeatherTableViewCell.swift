8ffbec4f2bebb29c3cca5ea7d06417692fdcdc2a
yujin <ruy203@gmail.com>
2024-04-23 11:45:31 +0900

    reuseIdentifier 수정

--- WeatherForecast/WeatherForecast/Weather/WeatherTableViewCell.swift
+++ WeatherForecast/WeatherForecast/Weather/WeatherTableViewCell.swift
@@ -7,7 +7,7 @@
 import UIKit
 
 class WeatherTableViewCell: UITableViewCell {
-    static let reuseIdentifier = "WeatherCell"
+    static let reuseIdentifier = String(describing: WeatherTableViewCell.self)
     
     var weatherIcon: UIImageView!
     var dateLabel: UILabel!
